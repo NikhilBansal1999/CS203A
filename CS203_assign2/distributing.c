198a4f6cce34bd697b479c2a11251f2dd1e4dd02
NIKHIL <nikhil.accent@gmail.com>
2018-08-25 10:29:58 +0000

    Added assignment 2 files

---
+++ CS203_assign2/distributing.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_ITERATIONS 1000
+#define NUM_SERVERS 10000000
+
+int main()
+{
+  //Z can be a maximum of NUM_SERVERS , beginning from 1
+  int* dist_count=(int*)calloc(NUM_SERVERS,sizeof(int));
+  for(int i=0;i<MAX_ITERATIONS;i++)
+  {
+    srandom(i);
+    if(i%100==0)
+    {
+      printf("%d iterations have finished.\n",i);
+    }
+    int max_job=0;
+    int* server_job_count=(int*)calloc(NUM_SERVERS,sizeof(int));
+    //for each client assign its job randomly to any server
+    //increment the job count of server by one
+    //Also keeps track of the maximum number of jobs any server has
+    for(int client=0;client<NUM_SERVERS;client++)//number of client is equal to number of servers
+    {
+      int server_number=random()%NUM_SERVERS;
+      *(server_job_count+server_number)=*(server_job_count+server_number)+1;
+      if(*(server_job_count+server_number) > max_job)
+      {
+        max_job=*(server_job_count+server_number);
+      }
+    }
+    //we have the maximum number of job any client has
+    free(server_job_count);
+    *(dist_count+max_job-1)=*(dist_count+max_job-1)+1;
+  }
+  FILE* dist_data=fopen("Distributed_Data.txt","w");
+  for(int i=0;i<NUM_SERVERS;i++)
+  {
+    fprintf(dist_data,"%d\n",*(dist_count+i));
+  }
+  fclose(dist_data);
+  free(dist_count);
+  return 0;
+}
---
+++ CS203_assign2/news.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_ITERATIONS 1000
+#define NUM_PEOPLE 1000000
+
+int main()
+{
+  //Probability of X being greater than 10*NUM_PEOPLE is very small
+  int* day_count=(int*)calloc(10*NUM_PEOPLE,sizeof(int));
+  int num_exceed=0;
+  for(int i=0;i<MAX_ITERATIONS;i++)
+  {
+    srandom(i);
+    if(i%100==0)
+    {
+      printf("%d iterations have finished.\n",i);
+    }
+    int* known_people=(int*)calloc(NUM_PEOPLE,sizeof(int));
+    *known_people=1;
+    int num_known=1;
+    int days=0;
+    while(num_known<NUM_PEOPLE)
+    {
+      int* set_in_this_iter=(int*)calloc(NUM_PEOPLE,sizeof(int));
+      for(int j=0;j<NUM_PEOPLE;j++)
+      {
+        if((*(known_people+j)==1)&&(*(set_in_this_iter+j)==0))//if j'th person knew the news the previous day
+        {
+          int random_person;
+          do//choose a random person not equal to j
+          {
+            random_person=random()%NUM_PEOPLE;
+          } while(random_person==j);
+          if(*(known_people+random_person)==0)
+          {
+            num_known=num_known+1;
+            *(known_people+random_person)=*(known_people+random_person)+1;
+            *(set_in_this_iter+random_person)=*(set_in_this_iter+random_person)+1;
+          }
+        }
+      }
+      free(set_in_this_iter);
+      days=days+1;
+    }
+    if(days>=(10*NUM_PEOPLE))
+    {
+      num_exceed=num_exceed+1;
+    }
+    else
+    {
+      *(day_count+days)=*(day_count+days)+1;
+    }
+    free(known_people);
+  }
+  FILE* day_data=fopen("News_Data.txt","w");
+  for(int i=0;i<10*NUM_PEOPLE;i++)
+  {
+    fprintf(day_data,"%d\n",*(day_count+i));
+  }
+  fprintf(day_data,"%d days exceed 10 times the number of people\n",num_exceed);
+  fclose(day_data);
+  free(day_count);
+  return 0;
+}
